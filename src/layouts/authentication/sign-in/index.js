4e966a4753ff37dd2cf9a5fd13a39648cbc9f0a7
RexGhub <rexford.addo26@gmail.com>
2024-09-21 20:45:37 +0000

    Project uploaded
    Login done so far

---
+++ src/layouts/authentication/sign-in/index.js
@@ -0,0 +1,124 @@
+/**
+=========================================================
+* Soft UI Dashboard React - v4.0.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import { useState } from "react";
+
+// react-router-dom components
+import { Link } from "react-router-dom";
+
+// @mui material components
+import Switch from "@mui/material/Switch";
+
+// Soft UI Dashboard React components
+import SoftBox from "components/SoftBox";
+import SoftTypography from "components/SoftTypography";
+import SoftInput from "components/SoftInput";
+import SoftButton from "components/SoftButton";
+import axios from 'axios';
+
+// Authentication layout components
+import CoverLayout from "layouts/authentication/components/CoverLayout";
+
+// Images
+import curved9 from "assets/images/curved-images/curved-6.jpg";
+
+const client = axios.create({
+  baseURL: "http://172.20.10.2:9090/" 
+});
+
+
+function SignIn() {
+  const [rememberMe, setRememberMe] = useState(true);
+  const [email,setEmail]=useState('')
+  const [password,setPassword]=useState('')
+
+  const handleSetRememberMe = () =>{ 
+    console.log("remember me event listener triggered");
+    setRememberMe(!rememberMe)};
+
+    const handleLogin = async (e) => {
+      e.preventDefault();
+      try {
+          const response = await client.post('users', { email, password });
+          console.log("response from login "+response.data)
+          // Store the token or user data in localStorage or state
+          localStorage.setItem('token', response.data.token);
+          // Redirect or handle successful login
+      } catch (error) {
+        console.log("there is an error reaching backend "+error)
+          // setError('Login failed. Please check your credentials.');
+      }
+  };
+
+  return (
+    <CoverLayout
+      title="Welcome back"
+      description="Enter your email and password to sign in"
+      image={curved9}
+    >
+      <SoftBox component="form" role="form">
+        <SoftBox mb={2}>
+          <SoftBox mb={1} ml={0.5}>
+            <SoftTypography component="label" variant="caption" fontWeight="bold">
+              Email
+            </SoftTypography>
+          </SoftBox>
+          <SoftInput type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+        </SoftBox>
+        <SoftBox mb={2}>
+          <SoftBox mb={1} ml={0.5}>
+            <SoftTypography component="label" variant="caption" fontWeight="bold">
+              Password
+            </SoftTypography>
+          </SoftBox>
+          <SoftInput type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </SoftBox>
+        <SoftBox display="flex" alignItems="center">
+          <Switch checked={rememberMe} onChange={handleSetRememberMe} />
+          <SoftTypography
+            variant="button"
+            fontWeight="regular"
+            onClick={handleSetRememberMe}
+            sx={{ cursor: "pointer", userSelect: "none" }}
+          >
+            &nbsp;&nbsp;Remember me
+          </SoftTypography>
+        </SoftBox>
+        <SoftBox mt={4} mb={1}>
+          <SoftButton variant="gradient" color="info" fullWidth onClick={handleLogin}>
+            sign in
+          </SoftButton>
+        </SoftBox>
+        <SoftBox mt={3} textAlign="center">
+          <SoftTypography variant="button" color="text" fontWeight="regular">
+            Don&apos;t have an account?{" "}
+            <SoftTypography
+              component={Link}
+              to="/authentication/sign-up"
+              variant="button"
+              color="info"
+              fontWeight="medium"
+              textGradient
+            >
+              Sign up
+            </SoftTypography>
+          </SoftTypography>
+        </SoftBox>
+      </SoftBox>
+    </CoverLayout>
+  );
+}
+
+export default SignIn;
